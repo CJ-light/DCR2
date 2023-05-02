075c50b4f7024cdd2d094bcddf25d6761b508671
RickJara <60295569+RickJara@users.noreply.github.com>
2023-05-01 19:10:42 -0500

    Revert "Camera"
    This reverts commit b82dde79c77df669eaf300ffa52aae8abe833a5c.

--- Assets/Scripts/UI/PressButtonGamepad.cs
+++ Assets/Scripts/UI/PressButtonGamepad.cs
@@ -8,21 +8,16 @@ public class PressButtonGamepad : MonoBehaviour
 {
     public PlayerInput _playerInput;
     public Button yourButton;
-    public CameraMovement scriptCamara;
     // Start is called before the first frame update
     void Start()
     {
         _playerInput = GetComponent<PlayerInput>();
         Button btn = yourButton.GetComponent<Button>();
-        scriptCamara.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_playerInput.actions["Start"].WasPressedThisFrame()) {
-            yourButton.onClick.Invoke();
-            scriptCamara.enabled = true;
-        }
+        if (_playerInput.actions["Start"].WasPressedThisFrame()) {yourButton.onClick.Invoke();}
     }
 }
